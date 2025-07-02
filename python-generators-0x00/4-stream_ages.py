4e5306e2458daa932b9eed98fe052e4abfdf9c77
Dario Didi <dididario.dd2@gmail.com>
2025-07-03 00:16:15 +0300

    fix: avg formula

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -4,21 +4,25 @@ seed = __import__('seed')
 def stream_user_ages():
     conn = seed.connect_to_prodev()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM user_data")
+    cursor.execute("SELECT * FROM user_data;")
+    # print(cursor.execute("SELECT AVG(age) FROM user_data;"))
     rows = cursor.fetchall()
 
     for row in rows:
         yield row['age']
+       # print(row)
 
     conn.close()
 
 
 if __name__ == '__main__':
     avg = 0
+    count = 1
     res = stream_user_ages()
 
     # while res is not None:
     for num in res:
-        avg = (avg + num)/2
-
+        avg += num
+        count += 1
+    avg /= count
     print(f"Average age of users: {avg}")
