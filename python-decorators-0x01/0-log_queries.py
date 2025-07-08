c2e62398c29a2e78d7edfc0eaf7be04b703d2706
Dario Didi <dididario.dd2@gmail.com>
2025-07-08 12:32:20 +0300

    modify query logger to use logger and datetime

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,14 +1,35 @@
 import sqlite3
 import functools
-
+import logging
+from datetime import datetime
 # decorator to lof SQL queries
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=('%(asctime)s - %(levelname)s - %(message)s')
+)
+
+logger = logging.getLogger('query_logger')
 
 
 def log_queries(func):
     @functools.wraps(func)
     def wrapper_log_query(*args, **kwargs):
-        print(kwargs['query'])
-        func(*args, **kwargs)
+        q = kwargs['query']
+        start = datetime.now()
+        logger.info(
+            f"Executing query: {q} [Started at {start.strftime('%Y-%m-%d %H:%M:%S.%f')}]")
+        try:
+            res = func(*args, **kwargs)
+            end = datetime.now()
+            duration = (end - start).total_seconds()
+            logger.info(
+                f"Query completed successfully in {duration:.4f} seconds [Rows affected: {len(res) if res is not None else 0}]")
+
+        except sqlite3.OperationalError as err:
+            logger.error(f"Query failed: {str(err)} [Query: {q}]")
+            raise
+
     return wrapper_log_query
 
 
