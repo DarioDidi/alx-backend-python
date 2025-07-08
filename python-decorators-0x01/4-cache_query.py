e98d8c465ba13c94b6b5080db413ea3992dcc546
Dario Didi <dididario.dd2@gmail.com>
2025-07-08 12:18:27 +0300

    add cache query decorator

--- python-decorators-0x01/4-cache_query.py
+++ python-decorators-0x01/4-cache_query.py
@@ -3,17 +3,39 @@ import time
 import sqlite3
 import functools
 
-'''
- create a decorator that caches the results of a database queries inorder to avoid redundant calls
 
-Instructions:
+def with_db_connection(func):
+    print("in with db func name", func.__name__)
+    """Decorator that automatically handles opening and closing database connections"""
 
-    Complete the code below by implementing a decorator cache_query(func) that caches query results based on the SQL query string
-'''
+    @functools.wraps(func)
+    def connect_db(*args, **kwargs):
+        # try partial func
+        conn = sqlite3.connect('users.db', False, isolation_level=None)
+        try:
+            return func(conn, *args, **kwargs)
+        except sqlite3.OperationalError as err:
+            print("transaction/connection error:", err)
+        finally:
+            conn.close()
+    return connect_db
 
+
+'''create a decorator that caches the results of a database queries inorder to avoid redundant calls'''
 query_cache = {}
 
-"""your code goes here"""
+
+def cache_query(func):
+    def wrapper(*args, **kwargs):
+        q = kwargs['query']
+        if q in query_cache.keys():
+            print("returning cached")
+            return query_cache[q]
+        else:
+            print("first time, caching query")
+            query_cache[q] = func(*args, **kwargs)
+            return query_cache[q]
+    return wrapper
 
 
 @with_db_connection
@@ -26,6 +48,8 @@ def fetch_users_with_cache(conn, query):
 
 # First call will cache the result
 users = fetch_users_with_cache(query="SELECT * FROM users")
-
+print(users[:5])
 # Second call will use the cached result
 users_again = fetch_users_with_cache(query="SELECT * FROM users")
+print(users_again[:5])
+print("VALID?", users[:5] == users_again[:5])
