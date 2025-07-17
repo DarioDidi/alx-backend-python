d065bbceceae50e79033618a642528e77be2248d
Dario Didi <dididario.dd2@gmail.com>
2025-07-17 07:05:35 +0300

    fix message get in conversation serializer

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,12 +1,8 @@
 import uuid
 from django.utils.translation import gettext_lazy as _
-
-from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import AbstractUser
 
-"""import uuid", "email", "password", "user_id", "first_name", "last_name", "phone_number", "primary_key"""
+from django.db import models
 
 
 class Users(AbstractUser):
@@ -20,17 +16,18 @@ class Users(AbstractUser):
 
 class Conversation(models.Model):
     # users = models.ManyToManyField(Users)
-    conversation_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    conversation_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
     participants = models.ManyToManyField(Users)
-
-
-"""message_id", "message_body", "sent_at", "created_at"""
 
 
 class Message(models.Model):
     sender = models.ForeignKey(Users, on_delete=models.CASCADE)
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
-    message_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    message_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
     message_body = models.TextField()
     sent_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,21 +1,31 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
-from models import Conversation, Message, Users
+from .models import Conversation, Message, Users
 
 
-class UserSerializer(ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ("first_name", "last_name", "phone_number", "id")
 
 
-class ConversationSerializer(ModelSerializer):
+class ConversationSerializer(serializers.ModelSerializer):
+    messages = serializers.SerializerMethodField()
+    id = serializers.CharField(max_length=80)
+
     class Meta:
         model = Conversation
-        fields = "__all__"
+
+        fields = ["conversationid", "participants", "created_at", "messages"]
+
+    def get_messages(self, obj):
+        ret = Message.objects.filter(conversation=obj)
+        if not len(ret):
+            raise serializers.ValidationError("Empty conversation")
+        return MessageSerializer(ret, many=True).data
 
 
-class MessageSerializer(ModelSerializer):
+class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = "__all__"
