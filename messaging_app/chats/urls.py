e765118a6770e31b0f2c848518f639c44e3204f0
Dario Didi <dididario.dd2@gmail.com>
2025-07-17 09:12:53 +0300

    add viewsets and urls

---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,9 @@
+# from django.urls import include, path
+#
+# from messaging_app.chats.views import ConversationViewSet
+#
+#
+# url_patterns = [
+#    path("new/conversation/", ConversationViewSet.as_view()),
+#    path("new/message/"),
+# ]
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.contrib.admin.utils import lookup_field
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from chats.serializers import (
+    ConversationSerializer,
+    MessageSerializer,
+)
+
+from .models import Message, Conversation
+
+
+class MessageViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing or retrieving messages.
+    """
+
+    lookup_field = "message_uuid"
+    serializer_class = MessageSerializer
+
+    def list(self, req, pk=None):
+        queryset = Message.objects.filter(conversation=pk)
+        serializer = MessageSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        msg = get_object_or_404(Message, pk=pk)
+        serializer = MessageSerializer(msg)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action in ("list", "create", "retrieve"):
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+    # def get_serializer(self):
+    #    return MessageSerializer(data)
+
+
+class ConversationViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing or retrieving conversation messages.
+    """
+
+    lookup_field = "conversation_uuid"
+    serializer_class = ConversationSerializer
+
+    def retrieve(self, request, pk=None):
+        convo = get_object_or_404(Conversation, pk=pk)
+        serializer = ConversationSerializer(convo)
+        return Response(serializer.data)
+
+    def create(self, req):
+        serializer = self.get_serializer(data=req.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save
