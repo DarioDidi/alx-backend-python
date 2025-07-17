17440c1a5ab28d9eb2eea91c858c7df36f10f4df
Dario Didi <dididario.dd2@gmail.com>
2025-07-17 09:26:27 +0300

    add response codes to creat in viewset

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,9 +1,10 @@
 from django.contrib.admin.utils import lookup_field
 from django.shortcuts import render, get_object_or_404
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import filters
 
 from chats.serializers import (
     ConversationSerializer,
@@ -20,11 +21,16 @@ class MessageViewSet(viewsets.ViewSet):
 
     lookup_field = "message_uuid"
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter]
+    filterset_fields = ["sender", "message_body"]
 
     def list(self, req, pk=None):
         queryset = Message.objects.filter(conversation=pk)
         serializer = MessageSerializer(queryset, many=True)
         return Response(serializer.data)
+        # return self.get_paginated_response(
+        #    self.paginate_queryset(serializer.data)
+        # )
 
     def retrieve(self, request, pk=None):
         msg = get_object_or_404(Message, pk=pk)
@@ -35,6 +41,7 @@ class MessageViewSet(viewsets.ViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def get_permissions(self):
         """
@@ -57,6 +64,8 @@ class ConversationViewSet(viewsets.ViewSet):
 
     lookup_field = "conversation_uuid"
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
+    filterset_fields = ["participants"]
 
     def retrieve(self, request, pk=None):
         convo = get_object_or_404(Conversation, pk=pk)
@@ -66,4 +75,5 @@ class ConversationViewSet(viewsets.ViewSet):
     def create(self, req):
         serializer = self.get_serializer(data=req.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save
+        serializer.save(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
