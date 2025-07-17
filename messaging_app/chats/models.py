afb38767872ec07fe9d876a5b4f546c50ee54a82
Dario Didi <dididario.dd2@gmail.com>
2025-07-17 06:33:40 +0300

    fix message and conversation fields

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -19,9 +19,18 @@ class Users(AbstractUser):
 
 
 class Conversation(models.Model):
-    users = models.ManyToManyField(Users)
+    # users = models.ManyToManyField(Users)
+    conversation_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(Users)
+
+
+"""message_id", "message_body", "sent_at", "created_at"""
 
 
 class Message(models.Model):
     sender = models.ForeignKey(Users, on_delete=models.CASCADE)
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    message_id = models.UUIDField(defaul=uuid.uuid4, editable=False)
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
