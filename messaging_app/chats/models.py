b6389b635b46b4e572adaf6644d61ca0a1a533fd
Dario Didi <dididario.dd2@gmail.com>
2025-07-17 12:59:05 +0300

    rename user model id field to user_id

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -7,7 +7,9 @@ from django.db import models
 
 class Users(AbstractUser):
     email = models.EmailField(unique=True)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
     first_name = models.CharField(blank=False, max_length=60)
     last_name = models.CharField(blank=False, max_length=60)
     phone_number = models.CharField(blank=False, max_length=13)
