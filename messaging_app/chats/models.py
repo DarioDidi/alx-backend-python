6a1a8c4c42131a77c339956aa2c25f24ae865bb1
Dario Didi <dididario.dd2@gmail.com>
2025-07-17 06:27:33 +0300

    add user fields for checker

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,17 +1,27 @@
+import uuid
+from django.utils.translation import gettext_lazy as _
+
 from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 
+"""import uuid", "email", "password", "user_id", "first_name", "last_name", "phone_number", "primary_key"""
+
 
 class Users(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(blank=False, max_length=60)
+    last_name = models.CharField(blank=False, max_length=60)
+    phone_number = models.CharField(blank=False, max_length=13)
+    password = models.CharField(_("password"), max_length=128)
 
 
-class conversation(models.Model):
+class Conversation(models.Model):
     users = models.ManyToManyField(Users)
 
 
-class message(models.Model):
+class Message(models.Model):
     sender = models.ForeignKey(Users, on_delete=models.CASCADE)
-    conversation = models.ForeignKey(conversation, on_delete=models.CASCADE)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
