d3abe3695bff70e98ca8fd80b4d6bcc8f1eb7412
Dario Didi <dididario.dd2@gmail.com>
2025-07-17 12:53:15 +0300

    move nested routing to chats.urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,14 +1,35 @@
 from django.urls import include, path
 from chats.views import ConversationViewSet, MessageViewSet
 from rest_framework import routers
+from rest_framework_nested.routers import NestedDefaultRouter
 
-router = routers.DefaultRouter()
-router.register(
+
+# main_router = routers.DefaultRouter()
+# main_router.register(
+#    r"conversations", ConversationViewSet, basename="conversations"
+# )
+
+app_router = routers.DefaultRouter()
+app_router.register(
     r"conversations", ConversationViewSet, basename="conversations"
 )
-router.register(r"messages", MessageViewSet, basename="messages")
+
+"""crate conversations router first based on default router"""
+# conversations_router = NestedDefaultRouter(
+#    main_router, r"conversations", lookup="conversation"
+# )
+conversations_router = NestedDefaultRouter(
+    app_router, r"conversations", lookup="conversation"
+)
+
+"""nest messages routing inside conversations"""
+conversations_router.register(
+    r"messages", MessageViewSet, basename="conversation-messages"
+)
 
 
 urlpatterns = [
-    path("", include((router.urls, "chats"), namespace="chats")),
+    # path("", include((main_router.urls, "chats"), namespace="chats")),
+    path("", include(app_router.urls)),
+    path("", include(conversations_router.urls)),
 ]
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -4,29 +4,12 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework_nested.routers import NestedDefaultRouter
 
-from chats.views import ConversationViewSet, MessageViewSet
-
-
-main_router = routers.DefaultRouter()
-main_router.register(
-    r"conversations", ConversationViewSet, basename="conversations"
-)
-
-"""crate conversations router first based on default router"""
-conversations_router = NestedDefaultRouter(
-    main_router, r"conversations", lookup="conversation"
-)
-"""nest messages routing inside conversations"""
-conversations_router.register(
-    r"messages", MessageViewSet, basename="conversation-messages"
-)
+# from chats.views import ConversationViewSet, MessageViewSet
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("chats.urls")),
-    path("api/", include(main_router.urls)),
-    path("api/", include(conversations_router.urls)),
     path(
         "api-auth/", include("rest_framework.urls", namespace="rest_framework")
     ),
