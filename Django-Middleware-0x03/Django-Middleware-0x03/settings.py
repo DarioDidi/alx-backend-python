8e77a7daccb9804446b01d2c6194c1ee8220ee44
Dario Didi <dididario.dd2@gmail.com>
2025-07-27 09:52:33 +0300

    add classes to settings

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # my own
     "chats.middleware.RequestLoggingMiddleware",
+    "chats.middleware.RestrictAccessByTimeMiddleware",
+    "chats.middleware.OffensiveLanguageMiddleware",
 ]
 
 ROOT_URLCONF = "messaging_app.urls"
